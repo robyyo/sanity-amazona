da278606d866bb54007cd6296935385ebb83535e
Robert Finkley <me@robertfinkley.com>
2022-05-15 12:21:08 -0400

    Creating darkMode context

--- utils/client.js
+++ utils/client.js
@@ -4,7 +4,7 @@ import config from './config';
 const client = sanityClient({
   projectId: config.projectId,
   dataset: config.dataset,
-  apiVersion: '2021-08-31',
+  apiVersion: '2021-10-21',
   useCdn: true,
 });
 
